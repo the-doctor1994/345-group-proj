3089489e8048de98c848ffef3290d261fd1dd759
Grayson Kempster <ekempste@umass.edu>
2015-11-16 23:52:25 -0500

    implemented the login auth

--- Query.java
+++ Query.java
@@ -34,8 +34,8 @@ public class Query {
     private PreparedStatement _director_mid_statement;
 
     /* uncomment, and edit, after your create your own customer database */
-    /*
-    private String _customer_login_sql = "SELECT * FROM customers WHERE login = ? and password = ?";
+    
+    private String _customer_login_sql = "SELECT * FROM accounts WHERE login = ? and password = ?";
     private PreparedStatement _customer_login_statement;
 
     private String _begin_transaction_read_write_sql = "BEGIN TRANSACTION READ WRITE";
@@ -46,8 +46,7 @@ public class Query {
 
     private String _rollback_transaction_sql = "ROLLBACK TRANSACTION";
     private PreparedStatement _rollback_transaction_statement;
-     */
-
+    
     public Query() {
     }
 
@@ -94,12 +93,12 @@ public class Query {
         _director_mid_statement = _imdb.prepareStatement(_director_mid_sql);
 
         /* uncomment after you create your customers database */
-        /*
+        
         _customer_login_statement = _customer_db.prepareStatement(_customer_login_sql);
         _begin_transaction_read_write_statement = _customer_db.prepareStatement(_begin_transaction_read_write_sql);
         _commit_transaction_statement = _customer_db.prepareStatement(_commit_transaction_sql);
         _rollback_transaction_statement = _customer_db.prepareStatement(_rollback_transaction_sql);
-         */
+         
 
         /* add here more prepare statements for all the other queries you need */
         /* . . . . . . */
@@ -143,18 +142,18 @@ public class Query {
         /* authenticates the user, and returns the user id, or -1 if authentication fails */
 
         /* Uncomment after you create your own customers database */
-        /*
+        
         int cid;
 
         _customer_login_statement.clearParameters();
-        _customer_login_statement.setString(1,name);
+        _customer_login_statement.setString(1,username);
         _customer_login_statement.setString(2,password);
         ResultSet cid_set = _customer_login_statement.executeQuery();
         if (cid_set.next()) cid = cid_set.getInt(1);
         else cid = -1;
         return(cid);
-         */
-        return (55);
+         
+        //return (55);
     }
 
     public void transaction_personal_data(int cid) throws Exception {
